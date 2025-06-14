8ce11b978dd27a64d39a54b4f5c230d440921acf
adibcoding <muhammadadibhasany1501@gmail.com>
2025-06-14 23:49:32 +0700

    feat: add credit system

---
+++ src/CampusCreditSystem.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "./CampusToken.sol";
+
+contract CampusCreditSystem {
+    CampusToken public token;
+    address public owner;
+
+    uint256 public constant VIBE_PER_MONAD = 1000;
+
+    error ZeroAmount();
+
+    event TopUp(address indexed user, uint256 monadAmount, uint256 vibeAmount);
+
+    constructor(address _address) {
+        token = CampusToken(_address);
+        owner = msg.sender;
+    }
+
+    // 🔼 Top up by sending native MONAD token
+    function topUpWithMonad() public payable {
+        if (msg.value == 0) revert ZeroAmount();
+
+        uint256 vibeAmount = msg.value * VIBE_PER_MONAD;
+
+        // Mint token
+        token.mintVibeToken(msg.sender, vibeAmount);
+
+        emit TopUp(msg.sender, msg.value, vibeAmount);
+    }
+
+    receive() external payable {
+      topUpWithMonad();
+    }
+
+}
--- src/CampusToken.sol
+++ src/CampusToken.sol
@@ -2,5 +2,42 @@
 pragma solidity ^0.8.26;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract CampusToken {}
+
+contract CampusToken is ERC20, AccessControl{
+  // Role
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  // owner/admin address
+  address public owner;
+  uint256 public constant initSupply = 10_000_000;
+  uint256 public constant maxSupply = 100_000_000;
+
+  // Error types
+  error ERC20MaxSupplyReached(address sender, address receiver, uint256 amount);
+
+  constructor() ERC20("Vibe Campus Token", "VIBT") {
+    owner = msg.sender;
+    _mint(msg.sender, initSupply);
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _grantRole(MINTER_ROLE, msg.sender);
+  }
+
+  function _update(address from, address to, uint256 value) internal override {
+    // Minting
+    if (from == address(0) && (totalSupply() + value > maxSupply)) {
+        revert ERC20MaxSupplyReached(from, to, value);
+    }
+
+    super._update(from, to, value);
+  }
+
+  function mintVibeToken(address receiver, uint256 amount) external onlyRole(MINTER_ROLE){
+    _mint(receiver, amount);
+  }
+
+  function grantNamedRole(address account, string memory roleName) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    _grantRole(keccak256(bytes(roleName)), account);
+  }
+}
